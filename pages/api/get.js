5c048e3e0842690b1ab7874864184dabd15a05b1
Ignacio Liotti <ignacio.liotti@devlights.com>
2022-07-01 16:19:02 -0300

    things

--- pages/api/get.js
+++ pages/api/get.js
@@ -5,8 +5,57 @@ export default async function getSocial(req, res) {
   try {
     
     await connectMongo();
-    const social = await Socials.find({social:req.body.post, post:req.body.date});
+    // const social = await Socials.find({social:req.body.post, post:req.body.date});
+
+    const { src, post, from, to } = req.query;
+
+    const social = await TimeRecord.aggregate([
+      {
+        $match: {
+          src: src,
+          post: post,
+          date: { $gte: new Date(from), $lt: new Date(to) },
+        },
+      },
+      // {
+      //   $lookup: {
+      //     from: "projects",
+      //     localField: "projectId",
+      //     foreignField: "_id",
+      //     as: "project",
+      //   },
+      // },
+      // {
+      //   $lookup: {
+      //     from: "members",
+      //     localField: "member",
+      //     foreignField: "_id",
+      //     as: "projectmember",
+      //   },
+      // },
+
+
+      // {
+      //   $group: {
+      //     _id: "$member",
+      //     records: {
+      //       $push: {
+      //         date: "$date",
+      //         from: "$from",
+      //         to: "$to",
+      //         hours: "$hours",
+      //         memberName: "$projectmember.name",
+      //         projectName: "$project.name",
+      //         recordDescription: "$description",
+      //       },
+      //     },
+      //   },
+      // },
+    ]);
+
     res.json({ social });
+
+
 
   } catch (error) {
 
