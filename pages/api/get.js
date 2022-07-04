e352f3797a1f1561d97d6c233cc9bdd2e24a48ad
Ignacio Liotti <ignacio.liotti@devlights.com>
2022-07-04 12:09:27 -0300

    things

--- pages/api/get.js
+++ pages/api/get.js
@@ -7,55 +7,73 @@ export default async function getSocial(req, res) {
     await connectMongo();
 
     // const social2 = await Socials.find({src:req.body.src});
+    const src = req.body.src ? 
+    await Socials.aggregate([{ 
+      $match: { src: req.body.src },
+      $group: {
+        _id: `${req.body.src ? req.body.src : '$src'}`,
+        count: { $sum: 1 }
+      }
+     }]) 
+    
+    : await Socials.aggregate([{ 
+      $group: {
+        _id: `${req.body.src ? req.body.src : '$src'}`,
+        count: { $sum: 1 }
+      }
+     }]) 
+    
 
     // const { src, post, from, to } = req.query;
 
-    const src = await Socials.aggregate([
+    // const src = await Socials.aggregate([
 
-      // if req.body.src truthy then match src, else do not match anything
-      { $match: { src: req.body.src } },
+    //   // if req.body.src truthy then match src, else do not match anything
+
+
+    //   { $match: { src: req.body.src } },
       
-      {
-        $group: {
-          _id: `${req.body.src ? req.body.src : '$src'}`,
-          count: { $sum: 1 }
-        }
-      }
-      // {
-      //   $lookup: {
-      //     from: "projects",
-      //     localField: "projectId",
-      //     foreignField: "_id",
-      //     as: "project",
-      //   },
-      // },
-      // {
-      //   $lookup: {
-      //     from: "members",
-      //     localField: "member",
-      //     foreignField: "_id",
-      //     as: "projectmember",
-      //   },
-      // },
+    //   {
+    //     $group: {
+    //       _id: `${req.body.src ? req.body.src : '$src'}`,
+    //       count: { $sum: 1 }
+    //     }
+    //   }
+    //   // {
+    //   //   $lookup: {
+    //   //     from: "projects",
+    //   //     localField: "projectId",
+    //   //     foreignField: "_id",
+    //   //     as: "project",
+    //   //   },
+    //   // },
+    //   // {
+    //   //   $lookup: {
+    //   //     from: "members",
+    //   //     localField: "member",
+    //   //     foreignField: "_id",
+    //   //     as: "projectmember",
+    //   //   },
+    //   // },
 
 
-      // {
-      //   $group: {
-      //     _id: "$member",
-      //     records: {
-      //       $push: {
-      //         date: "$date",
-      //         from: "$from",
-      //         to: "$to",
-      //         hours: "$hours",
-      //         memberName: "$projectmember.name",
-      //         projectName: "$project.name",
-      //         recordDescription: "$description",
-      //       },
-      //     },
-      //   },
-      // },
-    ]);
+    //   // {
+    //   //   $group: {
+    //   //     _id: "$member",
+    //   //     records: {
+    //   //       $push: {
+    //   //         date: "$date",
+    //   //         from: "$from",
+    //   //         to: "$to",
+    //   //         hours: "$hours",
+    //   //         memberName: "$projectmember.name",
+    //   //         projectName: "$project.name",
+    //   //         recordDescription: "$description",
+    //   //       },
+    //   //     },
+    //   //   },
+    //   // },
+    // ]);
     const posts = await Socials.aggregate([
       {
         $group: {
