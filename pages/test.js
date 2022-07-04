0ca7593500c1c1e707f52e5e54eb86082c5a0ea7
Ignacio Liotti <ignacio.liotti@devlights.com>
2022-07-04 14:50:35 -0300

    things

--- pages/test.js
+++ pages/test.js
@@ -1,13 +1,42 @@
 import React from 'react'
 import { useRouter } from 'next/router'
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+import { Pie } from 'react-chartjs-2';
 
+ChartJS.register(ArcElement, Tooltip, Legend);
 
 const Test = () => {
 
     const router = useRouter();
 
-
-    const socialFound = ['asda'];
+    const socialFound = [{
+        "both": {
+          "src": [
+            {
+              "_id": "faceboook",
+              "count": 7
+            },
+            {
+                "_id": "twitter",
+                "count": 12
+            },
+            {
+            "_id": "",
+            "count": 2
+            }
+          ],
+          "posts": [
+            {
+              "_id": "2do post",
+              "count": 2
+            },
+            {
+              "_id": "hola",
+              "count": 5
+            }
+          ]
+        }
+      }];
     const getData = async () => {
     const social = await fetch('/api/get', {
         method: 'POST',
@@ -26,8 +55,38 @@ const Test = () => {
         getData();
     }, [socialFound])
 
+    console.log(socialFound[0]?.both)
+    console.log(socialFound[0]?.both?.src)
+
+    const data = {
+        labels: socialFound[0]?.both?.src?.map(social => social._id),
+        datasets: [
+            {
+                data: socialFound[0]?.both?.src?.map(social => social.count),
+                backgroundColor: [
+                    '#FF6384',
+                    '#36A2EB',
+                    '#ffB2EB',
+                ],
+                hoverBackgroundColor: [
+                    '#FF6384',
+                    '#36A2EB',
+                    '#ffB2EB',
+                ]
+            }
+        ]
+    };
+    const options = {
+        legend: {
+            display: false
+        }
+    };
+    
+
     return (
-        <div>{socialFound}</div>
+        <div className='flex justify-center items-center w-[15rem]'>
+            <Pie data={data}/>
+        </div>
     )
 }
 
